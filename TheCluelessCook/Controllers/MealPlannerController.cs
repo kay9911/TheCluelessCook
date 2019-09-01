6e9c7f008d9cb5c19ef46b9c97c324b4342022d2
Kayne Padua <kay.padua@gmail.com>
2019-09-01 02:11:57 -0400

    Hooking up major pages, daily planner accordian implemented.

---
+++ TheCluelessCook/Controllers/MealPlannerController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TheCluelessCook.Data;
+using TheCluelessCook.Models.Planner;
+
+namespace TheCluelessCook.Controllers
+{
+    public class MealPlannerController : Controller
+    {
+        private readonly IRecipe _recipeService;
+
+        public MealPlannerController(IRecipe recipeService)
+        {
+            _recipeService = recipeService;
+        }
+
+        public IActionResult Index()
+        {
+            // Build out the week section section
+            DateTime _today = DateTime.Now;
+            int _currentDay = (int)_today.DayOfWeek;
+
+            // Get the list of days
+            var _WeekDetails = Enumerable.Range(0, 7)
+                .Select(Days => new DayDetailModel
+                {
+                    Id = _today.AddDays(Days).ToString(),
+                    Date = _today.AddDays(Days).Day.ToString(),
+                    Day = _today.AddDays(Days).DayOfWeek.ToString()
+                }).ToList();
+
+            var DaysofthisWeek = new MealPlannerIndexModel
+            {
+                days = _WeekDetails
+            };
+
+            return View(DaysofthisWeek);
+        }
+
+        public IActionResult Plan()
+        {
+            return View();
+        }
+    }
+}
---
+++ TheCluelessCook/Models/Planner/DayDetailModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheCluelessCook.Models.Planner
+{
+    public class DayDetailModel
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Day { get; set; }
+        public string Date { get; set; }
+    }
+}
